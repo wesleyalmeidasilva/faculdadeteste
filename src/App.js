ddb6ad4f232690326f76dabc0a5ed55b32bf7f77
Wesley Almeida <wesley.wow.002@hotmail.com>
2024-11-14 16:12:46 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
